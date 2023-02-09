c42fb22b0acca955d492a51c2a987bddf261e50d
Pallavic5 <pchedge235@gmail.com>
2023-02-10 02:01:48 +0530

    Added: Ability to ensure Valid contacts are added

--- Addressbook.js
+++ Addressbook.js
@@ -1,12 +1,11 @@
-/*Problem Statement UC1 :  Ability to create a Address Book
-Contact with first and last names,
-address, city, state, zip, phone number
-and email...
-- Program is written using IDE like VSS Code
-- Every UC is in a separate Git Branch and then merged with main
-- Naming Convention, Indentation, etc Code Hygiene will be checked during
-Review
-- Git Check In Comments and Version History will be monitored*/
+/*Problem Statement UC2:  Ability to ensure Valid
+Contacts are added... - First Name and Last Name should start with
+Capital and Minimum 3 Characters
+- Address, City and State should also have
+minimum 4 characters
+- Zip, Phone and Email should be valid as done in
+the Pattern Exercise
+- Throw Error if the RegEx test fails*/
 
 class ContactDetails {
     /**
@@ -45,4 +44,105 @@ class ContactDetails {
     toString() {
         return "First Name : " + this.firstName + "Last Name : " + this.lastName + "Address : " + this.address + "City : " + this.city + "State : " + this.state + "zip code : " + this.zip + "Phone Number : " + this.phoneNumber + "email : " + this.email;
     }
+}
+
+/**
+ * Create Array and ContactDetails Objects
+ */
+contactList = new Array();
+contact = new ContactDetails();
+function addDetails() {
+    /**
+     * Adding contact details to addressbook
+     */
+    contact.firstName = firstName('first name');
+    contact.lastName = firstName('last name');
+    contact.address = address('Address');
+    contact.city = address('city');
+    contact.state = address('State');
+    contact.zip = zip();
+    contact.phoneNumber = phoneNumber();
+    contact.email = email();
+    contactList.push(contact)
+}
+function printDetails() {
+    /**
+     * Display contact details
+     */
+    console.log(contactList);
+}
+function firstName(value) {
+    /**
+     * validation apply on details(first name and last name)
+     */
+    let pattern = new RegExp('^[A-Z]{1}[a-z]{2,20}$');
+    let input;
+    while (true) {
+        input = prompt("Enter " + value + " (first letter is upper case)");
+        let status = pattern.test(input);
+        if (status) {
+            break;
+        }
+    }
+    return input;
+}
+function address(value) {
+    /**
+     * validation apply on details(address, city and state)
+     */
+    let pattern = new RegExp('^[A-Z]{1}[a-z]{3,20}$');
+    let input;
+    while (true) {
+        input = prompt("Enter " + value + " (first letter is upper case)");
+        let status = pattern.test(input);
+        if (status) {
+            break;
+        }
+    }
+    return input;
+}
+function zip() {
+    /**
+     * validation apply on details( zip code)
+     */
+    let pattern = new RegExp('^[0-9]{6}$');
+    let input;
+    while (true) {
+        input = prompt("Enter zip code (Enter 6 numbers)");
+        let status = pattern.test(input);
+        if (status) {
+            break;
+        }
+    }
+    return input;
+}
+function phoneNumber() {
+    /**
+     * validation apply on details(mobile number)
+     */
+    let pattern = new RegExp('^[7-9]{1}[0-9]{9}$');
+    let input;
+    while (true) {
+        input = prompt("Enter Mobile Number (Enter 9 numbers)");
+        let status = pattern.test(input);
+        if (status) {
+            break;
+        }
+    }
+    return input;
+}
+function email() {
+    /**
+     * validation apply on details(email)
+     */
+    let pattern = new RegExp('^[A-Za-z]{3,20}[0-9]{0,10}[@]{1}[gmail.com]{9}$');
+    let input;
+    while (true) {
+        input = prompt("Enter email ");
+        let status = pattern.test(input);
+        if (status) {
+            break;
+        }
+    }
+    return input;
 }
